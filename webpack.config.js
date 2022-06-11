bf0ec050ce4d4574466f22f8ea7f133ff2a18f8a
hirotiia <hiroya_0610_15mro@me.com>
2022-06-12 00:55:31 +0900

    :wrench: Update environment  branch main

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, './dist/js'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
   },
   module: {
@@ -11,17 +13,26 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-            // 下から上に適応される
-            // [style-loader](/loaders/style-loader)
-          { loader: 'style-loader' },
+          // 下から上に適応される
+          { 
+              loader: MiniCssExtractPlugin.loader 
+          },
           // [css-loader](/loaders/css-loader)
           {
             loader: 'css-loader',
           },
           // [sass-loader](/loaders/sass-loader)
-          { loader: 'sass-loader' }
+          { 
+            loader: 'sass-loader' 
+          }
         ]
       }
     ]
-  }
+  },
+  plugins: [
+      new MiniCssExtractPlugin(),
+      new HtmlWebpackPlugin({
+          template: './src/index.html'
+      })
+  ]
 };
